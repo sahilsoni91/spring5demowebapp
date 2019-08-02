02cfadafdd2a5b929150dfcc980bfe4446f2104c
Sahil Soni <E01602@hsc.com>
2019-08-02 09:49:34 +0530

    Basic Web App

---
+++ src/main/java/com/springdemo/spring5demowebapp/Spring5demowebappApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.spring5demowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5demowebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5demowebappApplication.class, args);
+	}
+
+}
